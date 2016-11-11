9764cbf22ce7bf1f2f71998e89fafdc1be8afb3c
Vincent Appel <cxt155@porus01.sund.root.ku.dk>
2016-11-11 13:01:35 +0100

    debugging already

--- geno/extract.snps.from.vcf.wrapper.sh
+++ geno/extract.snps.from.vcf.wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chr=$1
-grep -wFf $chr.snps <(zcat imputed/$chr.vcf.gz) > $chr.snps.on.vcf
+grep -wFf $(dirname $0)/$chr.snps <(zcat $(dirname $0)/imputed/$chr.vcf.gz) > $(dirname $0)/$chr.snps.on.vcf
