a4d5607b2c6a82f7ee554c373c77cbd6613a66ae
Vincent Appel <cxt155@porus01.sund.root.ku.dk>
2016-11-11 14:53:45 +0100

    testing phase 1 done

--- 1.5_format_vcf.sh
+++ 1.5_format_vcf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-zcat geno/imputed/$(head -1 <(ls geno/imputed/)) | head -n $($(head -1 <(ls geno/imputed/)) | awk '$1=="#CHROM" {print FNR; exit}') > geno/VCFheader
-tail -1 VCFheader | sed 's/#//g' | sed 's/\t/"\t"/g' | sed 's/$/"/g' | sed 's/^/"/g' > newHeader
+zcat geno/imputed/$(head -1 <(ls geno/imputed/)) | awk '{print $0}; $1=="#CHROM" {exit}' > geno/VCFheader
+tail -1 geno/VCFheader | sed 's/#//g' | sed 's/\t/"\t"/g' | sed 's/$/"/g' | sed 's/^/"/g' > newHeader
 
 cat geno/VCFheader geno/*snps.on.vcf > geno/all.SNPs.vcf
 bcftools annotate -R QUAL,FILTER,INFO,FORMAT/GT,FORMAT/ADS,FORMAT/GP geno/all.SNPs.vcf | grep -v '#' > genoFile.noHead
