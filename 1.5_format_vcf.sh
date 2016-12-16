cc05d32fad65ff21450ca2022d59ca2d7dc208b7
Vincent Appel <cxt155@porus01.sund.root.ku.dk>
2016-12-16 14:29:41 +0100

    updated to bcftools 1.3.1

--- 1.5_format_vcf.sh
+++ 1.5_format_vcf.sh
@@ -3,6 +3,6 @@ zcat geno/imputed/$(head -1 <(ls geno/imputed/)) | awk '{print $0}; $1=="#CHROM"
 tail -1 geno/VCFheader | sed 's/#//g' | sed 's/\t/"\t"/g' | sed 's/$/"/g' | sed 's/^/"/g' > newHeader
 
 cat geno/VCFheader geno/*snps.on.vcf > geno/all.SNPs.vcf
-bcftools annotate -R QUAL,FILTER,INFO,FORMAT/GT,FORMAT/ADS,FORMAT/GP geno/all.SNPs.vcf | grep -v '#' > genoFile.noHead
+bcftools annotate -x QUAL,FILTER,INFO,FORMAT/GT,FORMAT/ADS,FORMAT/GP geno/all.SNPs.vcf | grep -v '#' > genoFile.noHead
 plink19 --vcf geno/all.SNPs.vcf --r2 --out geno/ldprune
 plink19 --vcf geno/all.SNPs.vcf --freq --out geno/maf
